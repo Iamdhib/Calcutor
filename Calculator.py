5c9e530213ff033221f7f59880a05d9b25438eb7
Iamdhib <113667221+Iamdhib@users.noreply.github.com>
2022-09-23 08:16:39 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,6 @@
 def calculator(no):
-    zx=12+5
-    while no!="q":
-        print(eval(no))
-        break
-
+    while True:
+        return eval(no)
 no=input("enter no's to calculate e.g(2+5 or 4/2) etc: ")
 sum=calculator(no)
+print(sum)
